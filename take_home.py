8380e0aef78c61480d719d9f9ab7fcad08903c69
julie-norris <julienorris0715@gmail.com>
2020-02-16 20:51:23 -0800

    working on wide to long

--- take_home.py
+++ take_home.py
@@ -28,20 +28,23 @@ def convert_long_to_wide():
 	df['ID']=df['CO_CODE']+df['DIST_CODE']+df['SCH_CODE']
 
 	"""change name from enrollment to OVERALL_ENROL and join the two dataframes"""
-	df1.rename(columns={"enrollment" : "OVERALL_ENROL"}, inplace=True)
+	"""df1.rename(columns={"enrollment" : "OVERALL_ENROL"}, inplace=True)
 	result = pd.concat([df, df1], axis=1, sort=False)
-	"""replace all NaN with 0 to be able to make calculations"""
+	replace all NaN with 0 to be able to make calculations
 	result['OVERALL_ENROL']=result['OVERALL_ENROL'].fillna(0)
 	result['state_id']=result['state_id'].fillna(0)
 	
-	"""Calculate the percentage of students taking the SAT at each school. Store that value
-in a separate column, PERC_TAKING_SAT, noting any irregularities in the result."""
+	Calculate the percentage of students taking the SAT at each school. Store that value
+	irregularities in a separate column, PERC_TAKING_SAT, noting any irregularities in the result.
 	result['PERC_TAKING_SAT'] = result['N_STUDENTS_SCORED'].astype(int)/result['OVERALL_ENROL'].astype(int)
 	result['PERC_TAKING_SAT'] = result['PERC_TAKING_SAT']*100
+	print(result) """
 
-	print(result)
-	new_table=pd.melt(df, id_vars['ID'], value_vars=['TOTAL'],var_name='DATE_TYPE') #,['MATHEMATICS'],['CRITICAL_READING'],['WRITING'],['SAT_1550'], 
-	print(new_table)
-	return(new_table)
+	"""reshape the SAT file from a wide format to a normalized (or long) structure
+	with the following three columns: ID, DATA_TYPE, and VALUE."""
+	df['indx']=df.index
+	print(df)
+	pd.melt(df, id_vars='ID', value_vars=['MATHEMATICS'],['CRITICAL_READING'],['WRITING'],['SAT_1550'],['TOTAL'])
+	print(df)
 
 convert_long_to_wide()
